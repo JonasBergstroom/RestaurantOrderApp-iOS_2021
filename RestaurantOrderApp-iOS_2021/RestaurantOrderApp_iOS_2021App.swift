19c50ae9a0a9d81d0896bdec6d048d267115778f
jonasbergstrom <jontrian97@gmail.com>
2021-02-16 16:12:16 +0100

    Initial Commit

---
+++ RestaurantOrderApp-iOS_2021/RestaurantOrderApp_iOS_2021App.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantOrderApp_iOS_2021App.swift
+//  RestaurantOrderApp-iOS_2021
+//
+//  Created by Jonas Bergström on 2021-02-16.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantOrderApp_iOS_2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
