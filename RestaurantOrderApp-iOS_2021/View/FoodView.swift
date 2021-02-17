55b6b55ed3851c0129075e6ae179e6c95fa96db8
jonasbergstrom <jontrian97@gmail.com>
2021-02-17 16:30:12 +0100

    Downloaded an image package to get the images from Firebase to the app

---
+++ RestaurantOrderApp-iOS_2021/View/FoodView.swift
@@ -0,0 +1,27 @@
+//
+//  FoodView.swift
+//  RestaurantOrderApp-iOS_2021
+//
+//  Created by Jonas Bergström on 2021-02-17.
+//
+
+import SwiftUI
+import SDWebImage
+
+struct FoodView: View {
+    
+    var item: Item
+    var body: some View {
+        
+        VStack {
+            
+            
+            
+            
+            
+        }
+        
+        
+        
+    }
+}
